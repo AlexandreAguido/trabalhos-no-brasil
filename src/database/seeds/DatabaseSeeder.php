51e7e7c30175f44d0b2ef3acfa05ef739a850e2a
AlexandreAguido <AlexandreAguido@users.noreply.github.com>
2019-08-23 14:46:44 -0300

    fix: menos queries necessarias para inserir uma vaga no banco

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -1,8 +1,8 @@
 <?php
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use App\Libraries\Geonames;
 use App\Libraries\GoogleDriveWrapper;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,38 +12,40 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
 
-    private function get_empresa_id($empresa, $email){
+    private function get_empresa_id($empresa, $email)
+    {
         // cria uma entrada no banco caso a empresa nao exista no banco
         // e retorna o seu id
-        if(!isset($empresa)){$empresa = '';}
+        if (!isset($empresa)) {$empresa = '';}
         $id = DB::table('empresas')->where('nome', $empresa)->value('id');
-        if(empty($id)){
+        if (empty($id)) {
             $data = ['nome' => $empresa];
-            if(isset($email)){$data['email'] = $email;}
+            if (isset($email)) {$data['email'] = $email;}
             $id = DB::table('empresas')->insertGetId($data);
         }
         return $id;
     }
 
-    private function get_locale_ids($cidade){
+    private function get_locale_ids($cidade)
+    {
         $geoname = new Geonames;
         $cidade_id = DB::table('cidades')->where('nome', $cidade)->value('id');
-        if( !isset( $cidade_id ) ){
-            $estado = $geoname->get_state_name_from_city($cidade) ;
-            if(!$estado){return false;}
+        if (!isset($cidade_id)) {
+            $estado = $geoname->get_state_name_from_city($cidade);
+            if (!$estado) {return false;}
             // criar estado se nao existir
             $estado_id = DB::table('estados')->where('nome', $estado)->value('id');
-            if( !isset( $estado_id ) ){
+            if (!isset($estado_id)) {
                 $uf = $geoname->get_uf_from_city($cidade);
-                $estado_id = DB::table('estados')->insertGetId( [
+                $estado_id = DB::table('estados')->insertGetId([
                     'nome' => $estado,
-                    'uf' => $uf
-                    ] );
+                    'uf' => $uf,
+                ]);
             }
             // criar cidade
             $data = [
-                'nome' => $cidade, 
-                'estado_id' => $estado_id
+                'nome' => $cidade,
+                'estado_id' => $estado_id,
             ];
             $cidade_id = DB::table('cidades')->insertGetId($data);
         }
@@ -51,55 +53,53 @@ class DatabaseSeeder extends Seeder
         return ['estado' => $locale_data->estado_id, 'cidade' => $locale_data->id];
     }
 
-    private function store_data($vagas_assoc){
+    private function store_data($vagas_assoc)
+    {
+        $data = [];
         foreach ($vagas_assoc as $vaga) {
             // verificar existencia da vaga
-            if( $vaga['cidade'] == "Não Informado" || DB::table('vagas')->where('vaga_url', $vaga['url'])->exists()){continue;}
+            if ($vaga['cidade'] == "Não Informado" || DB::table('vagas')->where('vaga_url', $vaga['url'])->exists()) {continue;}
 
-            $data = [];
             $locale_ids = $this->get_locale_ids($vaga['cidade']);
-            if(!$locale_ids){continue;} // vaga fora do brasil
-            $data['titulo'] = $vaga['titulo'];
-            $data['vaga_url'] = $vaga['url'];
-            $data['descricao'] = $vaga['descricao'];
-            $data['cidade_id'] = $locale_ids['cidade'];
-            $data['estado_id'] = $locale_ids['estado'];
+            if (!$locale_ids) {continue;} // vaga fora do brasil
+            $item['titulo'] = $vaga['titulo'];
+            $item['vaga_url'] = $vaga['url'];
+            $item['descricao'] = $vaga['descricao'];
+            $item['cidade_id'] = $locale_ids['cidade'];
+            $item['estado_id'] = $locale_ids['estado'];
             $email = isset($vaga['empresa_email']) ? $vaga['empresa_email'] : "";
-            $data['empresa_id'] = $this->get_empresa_id($vaga['empresa'], $email);
+            $item['empresa_id'] = $this->get_empresa_id($vaga['empresa'], $email);
 
             // variaveis opcionais
-            $vaga['salario'] == -1 ? $data['salario'] = 0 : $data['salario'] = $vaga['salario'];
-            if( isset( $vaga['quantidade'] ) ){ $data['quantidade']  = $vaga['quantidade']; }
-            if( isset( $vaga['pdc'] ) ){ $data['pdc'] = $vaga['pdc'] == 'null' ? 0 : 1;}
-            DB::table('vagas')->insert($data);
-            
+            $vaga['salario'] == -1 ? $item['salario'] = 0 : $item['salario'] = $vaga['salario'];
+            if (isset($vaga['quantidade'])) {$item['quantidade'] = $vaga['quantidade'];}
+            if (isset($vaga['pdc'])) {$item['pdc'] = $vaga['pdc'] == 'null' ? 0 : 1;}
+            array_push($data, $item);
         }
-        
+        DB::table('vagas')->insert($data);
 
     }
 
-
     public function run()
-    {   
-        $path = dirname( __FILE__ ) . '/../../storage/vagas/';
+    {
+        $path = dirname(__FILE__) . '/../../storage/vagas/';
         /* download file*/
         $drive = new GoogleDriveWrapper;
         $drive->download_file($path);
-        
+
         $has_vagas = DB::table('vagas')->exists();
         $files = scandir($path);
-        foreach($files as $f_name){
-            if($f_name == 'sample.json' && $has_vagas){continue;} // debug scrapper
+        foreach ($files as $f_name) {
             $strings = explode('.', $f_name);
             $file_extension = end($strings);
-            if($file_extension != 'json'){continue;}
+            if ($file_extension != 'json') {continue;}
             $f_name = $path . $f_name;
             $file = fopen($f_name, 'r');
-            $data  = json_decode(fgets($file), true);
+            $data = json_decode(fgets($file), true);
             $this->store_data($data);
-            if($f_name != 'sample.json') {unlink($f_name);}
+            unlink($f_name);
         }
-        
+
         $drive->delete_file();
     }
 }
